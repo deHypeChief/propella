8564b63c7d9f36e66388629d7bd440e7d351ab6e
deHypeChief <dev.hype7@gmail.com>
2023-10-28 13:13:24 +0100

    client update

--- client/src/utils/authForm.js
+++ client/src/utils/authForm.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 const apiKey = import.meta.env.VITE_API_URL;
 const evn = import.meta.env.VITE_NODE_ENV;
 
-const URL = evn == 'production'? `${apiKey}/api/users` : 'http://localhost:8080/api/users'
-
+const URL = `https://propella-iota.vercel.app/api/users`
+const L_url = 'http://localhost:8080/api/users'
+console.log(URL);
 
 export async function getCategory(callback){
     const API_URI = `${URL}/categories`
