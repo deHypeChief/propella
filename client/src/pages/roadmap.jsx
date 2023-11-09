34f6efdc621c9d9361de82dc2301bca0a17980db
deHypeChief <dev.hype7@gmail.com>
2023-11-09 06:58:34 +0100

    home update

--- client/src/pages/roadmap.jsx
+++ client/src/pages/roadmap.jsx
@@ -59,7 +59,7 @@ export default function Roadmap() {
             year: "Year 2024",
             month: "January ",
             detail: `Operation to commence by 2024`,
-            imgUrl:"-https://images.unsplash.com/photo-1484807352052-23338990c6c6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            imgUrl:"https://images.unsplash.com/photo-1484807352052-23338990c6c6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
     ]
 
